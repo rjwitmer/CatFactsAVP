b2c6501df4f8b2fa80f3aea54084bb962cbf2ada
Bob Witmer <bwitmer@golden.net>
2025-09-20 15:55:46 -0400

    Initial Commit

---
+++ CatFactsAVP/CatFactsAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsAVPApp.swift
+//  CatFactsAVP
+//
+//  Created by Bob Witmer on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
